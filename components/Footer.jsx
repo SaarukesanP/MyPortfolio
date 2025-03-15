e33e02ca96823c5d237d89dcb88fc6e33c5e7002
SaarukesanP <121214227+SaarukesanP@users.noreply.github.com>
2025-03-15 23:11:10 +0530

    contact and footer modified

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -8,20 +8,11 @@ const Footer = ({isDarkMode}) => {
       <div className='text-center'>
         <Image src={isDarkMode ? assets.logo_dark : assets.logo} alt='' className='w-36 mx-auto mb-2'/>
 
-        <div className='w-max flex items-center gap-2 mx-auto'>
+        <div className='w-max flex items-center gap-2 mx-auto pb-20'>
             <Image src={isDarkMode ? assets.mail_icon_dark : assets.mail_icon} alt='' className='w-6'/>
-            greatstackdev@gmail.com
+            saaru27kesan@gmail.com
         </div>
       </div>
-
-    <div className='text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6'>
-        <p>© 2025 GreatStack. All rights reserved.</p>
-        <ul className='flex items-center gap-10 justify-center mt-4 sm:mt-0'>
-            <li><a target='_blank' href="https://instagram.com/greatstackdev">GitHub</a></li>
-            <li><a target='_blank' href="https://instagram.com/greatstackdev">LinkedIn</a></li>
-            <li><a target='_blank' href="https://instagram.com/greatstackdev">Twitter</a></li>
-        </ul>
-    </div>
 
     </div>
   )
