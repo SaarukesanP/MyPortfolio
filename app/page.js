4516f0a1ba7fa0a2c8b825199337c484c6a5e910
SaarukesanP <121214227+SaarukesanP@users.noreply.github.com>
2025-03-18 06:24:08 +0530

    Final page updated

--- app/page.js
+++ app/page.js
@@ -8,30 +8,34 @@ import Header2 from "../components/Header2";
 import Navbar from "../components/Navbar";
 import Navbar2 from "../components/Navbar2";
 import Services from "../components/Services";
+import {AppleCardsCarouselDemo} from "../components/Projects";
 import Work from "../components/Work";
+import {LayoutGridDemo} from "../components/Work2";
 import {TimelineDemo} from "../components/Timeline";
+import {AnimatedTooltipPreview} from "../components/Skill";
+
 
 export default function Home() {
 
  const [isDarkMode, setIsDarkMode] = useState(false);
 
- useEffect(()=>{
-  if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    setIsDarkMode(true)
-  }else{
-    setIsDarkMode(false)
-  }
- },[])
-
- useEffect(()=>{
-    if(isDarkMode){
-      document.documentElement.classList.add('dark');
-      localStorage.theme = 'dark';
-    }else{
-      document.documentElement.classList.remove('dark');
-      localStorage.theme = '';
-    }
- },[isDarkMode])
+ // useEffect(()=>{
+ //  if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+ //    setIsDarkMode(true)
+ //  }else{
+ //    setIsDarkMode(false)
+ //  }
+ // },[])
+ //
+ // useEffect(()=>{
+ //    if(isDarkMode){
+ //      document.documentElement.classList.add('dark');
+ //      localStorage.theme = 'dark';
+ //    }else{
+ //      document.documentElement.classList.remove('dark');
+ //      localStorage.theme = '';
+ //    }
+ // },[isDarkMode])
 
   return (
     <>
@@ -39,8 +43,9 @@ export default function Home() {
     <Header2/>
     {/*<About isDarkMode={isDarkMode} />*/}
         <TimelineDemo/>
-    <Services/>
-    <Work/>
+    <AppleCardsCarouselDemo/>
+    <LayoutGridDemo/>
+        <AnimatedTooltipPreview/>
     <Contact/>
     <Footer/>
 
