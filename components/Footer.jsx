bd13c3bc53d55e3632e31f736ddf7dea7297d441
SaarukesanP <121214227+SaarukesanP@users.noreply.github.com>
2025-03-19 19:50:00 +0530

    Footer in First draft

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,15 +4,22 @@ import React from 'react'
 
 const Footer = ({isDarkMode}) => {
   return (
-    <div className='mt-20'>
-      <div className='text-center'>
-        <Image src={isDarkMode ? assets.logo_dark : assets.logo} alt='' className='w-36 mx-auto mb-2'/>
+    <div className="bg-blue-50 flex justify-center items-center">
+        <div className='text-center'>
+            <div className='w-max flex items-center gap-2 mx-auto'>
+                <Image src={assets.location} alt='' className='w-6'/>
+                2nd Lane, Market Road, Chenkalady, Batticaloa
+            </div>
+            <div className='w-max flex items-center gap-2 mx-auto'>
+                <Image src={assets.mail_icon} alt='' className='w-6'/>
+                saaru27kesan@gmail.com
+            </div>
 
-        <div className='w-max flex items-center gap-2 mx-auto pb-20'>
-            <Image src={isDarkMode ? assets.mail_icon_dark : assets.mail_icon} alt='' className='w-6'/>
-            saaru27kesan@gmail.com
+            <div className='w-max flex items-center gap-2 mx-auto pb-20'>
+                <Image src={assets.phone} alt='' className='w-6'/>
+                +94 76 472 2514
+            </div>
         </div>
-      </div>
 
     </div>
   )
