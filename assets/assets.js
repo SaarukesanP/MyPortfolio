32a378da5ff54b67d23285d19b8d91cbbc21a19a
SaarukesanP <121214227+SaarukesanP@users.noreply.github.com>
2025-03-19 19:49:02 +0530

    assets uploaded and updated

--- assets/assets.js
+++ assets/assets.js
@@ -41,6 +41,27 @@ import x from './x.png';
 import linkedin from './linkedin.png';
 import background from './background.jpg';
 import logo_1 from './logo_1.png';
+import logo_2 from './logo_2.png';
+import logo_3 from './logo_3.png';
+import logo_4 from './logo_4.png';
+import logo_5 from './logo_5.png';
+import logo_6 from './logo_6.png';
+import logo_7 from './logo_7.png';
+import logo_8 from './logo_8.png';
+import logo_9 from './logo_9.png';
+import logo_10 from './logo_10.png';
+import logo_11 from './logo_11.png';
+import logo_12 from './logo_12.png';
+import logo_13 from './logo_13.png';
+import logo_14 from './logo_14.png';
+import logo_15 from './logo_15.png';
+import logo_16 from './logo_16.png';
+import logo_17 from './logo_17.png';
+import logo_18 from './logo_18.png';
+import logo_19 from './logo_19.png';
+import phone from './phone.png';
+import location from './location.png';
+
 
 export const assets = {
     user_image,
@@ -85,7 +106,27 @@ export const assets = {
     instagram,
     x,
     background,
-    logo_1
+    logo_1,
+    logo_2,
+    logo_3,
+    logo_4,
+    logo_5,
+    logo_6,
+    logo_7,
+    logo_8,
+    logo_9,
+    logo_10,
+    logo_11,
+    logo_12,
+    logo_13,
+    logo_14,
+    logo_15,
+    logo_16,
+    logo_17,
+    logo_18,
+    logo_19,
+    phone,
+    location
 };
 
 export const workData = [
